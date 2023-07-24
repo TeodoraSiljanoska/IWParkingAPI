60dbfaf9d1cb580c0b2c2c2c80545350a5fa22e9
Maja Vuevska <maja.vuevska@gmail.com>
2023-07-24 16:21:03 +0200

    feat:create_igeneric_repository_and_iunit_of_work#1

---
+++ IWParkingAPI/Infrastructure/Repository/SQLRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IWParkingAPI.Infrastructure.Repository
+{
+    public class SQLRepository<TEntity, TContext> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private DbContext _context;
+        private DbSet<TEntity> _db;
+
+        public SQLRepository(DbContext context)
+        {
+            _context = context;
+            _db = context.Set<TEntity>();
+        }
+        public void Delete(TEntity obj)
+        {
+            _db.Remove(obj);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _db.ToList();
+        }
+
+        public TEntity GetById(object id)
+        {
+            return _db.Find(id);
+        }
+
+        public void Insert(TEntity obj)
+        {
+            _db.Add(obj);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity obj, TEntity objChanges)
+        {
+            _db.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+    }
+}
