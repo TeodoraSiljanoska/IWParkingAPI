ab7c749d000228a632170513a1b884446cbe64d1
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-08-03 11:55:11 +0200

    old user roles class deleted

--- IWParkingAPI/Models/Data/UserRoles.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IWParkingAPI.Models
-{
-    public static class UserRoles
-    {
-        public const string SuperAdmin = "SuperAdmin";
-        public const string User = "User";
-    }
-}
