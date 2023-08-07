de663bb039b424cf591de80c45f4254a95a70814
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-08-07 10:33:21 +0200

    adding columns to request table and parking lot table

--- IWParkingAPI/Models/UserRoles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IWParkingAPI.Models
-{
-    public static class UserRoles
-    {
-        public const string SuperAdmin = "SuperAdmin";
-        public const string User = "User";
-        public const string Owner = "ParkingOwner";
-    }
-}
