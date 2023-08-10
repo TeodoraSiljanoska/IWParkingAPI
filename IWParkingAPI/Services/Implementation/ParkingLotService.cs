597741a156fc936b7b65f989b6998181fa99df34
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-08-10 11:03:10 +0200

    optimized make parking lot favourite response

--- IWParkingAPI/Services/Implementation/ParkingLotService.cs
+++ IWParkingAPI/Services/Implementation/ParkingLotService.cs
@@ -169,7 +169,6 @@ namespace IWParkingAPI.Services.Implementation
 
             if (parkingLot.IsDeactivated == true)
             {
-               
                 _response.StatusCode = HttpStatusCode.NotModified;
                 _response.Message = "Parking lot is already deactivated";
                 _response.ParkingLot = parkingLotDTO;
@@ -191,7 +190,8 @@ namespace IWParkingAPI.Services.Implementation
         {
             var user = _userRepository.GetAsQueryable(x => x.Id == userId, null, x => x.Include(y => y.ParkingLotsNavigation)).FirstOrDefault();
             var parkingLot = _parkingLotRepository.GetById(parkingLotId);
-           
+            var parkingLotDTO = _mapper.Map<ParkingLotDTO>(parkingLot);
+
             if (user == null || user.IsDeactivated == true)
             {
                 _response.StatusCode = HttpStatusCode.NotFound;
@@ -208,14 +208,12 @@ namespace IWParkingAPI.Services.Implementation
 
             if (user.ParkingLotsNavigation.Contains(parkingLot))
             {
+               _response.ParkingLot = parkingLotDTO;
                _response.StatusCode = HttpStatusCode.Conflict;
-               _response.Message = "Parking Lot is already a favorite";
+               _response.Message = "Parking Lot is already favorite";
                return _response;
             }
-            
 
-
-            var parkingLotDTO = _mapper.Map<ParkingLotDTO>(parkingLot);
             user.ParkingLotsNavigation.Add(parkingLot);
             _userRepository.Update(user);
             _unitOfWork.Save();
