5ca09af92bcd44d57720c64ab8f9e2320ff7f462
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-08-15 17:01:03 +0200

    trying to implement fluent validations

---
+++ IWParkingAPI/Fluent Validations/ValidationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+using FluentValidation;
+
+namespace IWParkingAPI.Fluent_Validations
+{
+    public static class ValidationExtensions
+    {
+        public static ValidationModel RunValidation<T> (this T model)
+        {
+            var errors = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(model, new ValidationContext(model, null, null), errors, true);
+
+            return new ValidationModel
+            {
+                IsValid = isValid,
+                Errors = errors
+            };
+        }
+
+        public static IServiceCollection AddValidator(this IServiceCollection services)
+        {
+            services.AddSingleton<IValidateService, ValidateService>();
+
+            return services;
+        }
+    }
+}
