ba5486cd1056e6289f6985949d561b5845be0719
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-08-16 16:49:54 +0200

    Starting with fluent validations in vehicle service

---
+++ IWParkingAPI/Middleware/Fluent Validations/Validators/AddNewVehicleValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using IWParkingAPI.Models.Requests;
+
+namespace IWParkingAPI.Middleware.Fluent_Validations.Validators
+{
+    public class AddNewVehicleValidator : AbstractValidator<VehicleRequest> 
+    {
+        public AddNewVehicleValidator()
+        {
+            RuleFor(x => x.UserId)
+               .NotEmpty().WithMessage("UserId is required")
+               .GreaterThan(0).WithMessage("UserId should be greater than 0");
+
+            RuleFor(x => x.PlateNumber)
+               .NotEmpty()
+               .WithMessage("PlateNumber is required");
+
+            //      RuleFor(x => x.Type)
+            //         .NotEmpty()
+            //         .WithMessage("Type is required")
+            //         .Must("")
+        }
+    }
+}
