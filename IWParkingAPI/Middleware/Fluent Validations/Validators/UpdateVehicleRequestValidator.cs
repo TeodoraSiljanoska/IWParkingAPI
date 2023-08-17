72b6b4240515f28d31133bdb58976042cd4153de
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-08-17 14:32:19 +0200

    Fluent Validation in the VehicleService implemented, and also, all the methods in that service were optimized in what they were giving back as a response

--- IWParkingAPI/Middleware/Fluent Validations/Validators/UpdateVehicleRequestValidator.cs
+++ IWParkingAPI/Middleware/Fluent Validations/Validators/UpdateVehicleRequestValidator.cs
@@ -3,14 +3,10 @@ using IWParkingAPI.Models.Requests;
 
 namespace IWParkingAPI.Middleware.Fluent_Validations.Validators
 {
-    public class AddNewVehicleValidator : AbstractValidator<VehicleRequest> 
+    public class UpdateVehicleRequestValidator : AbstractValidator<UpdateVehicleRequest>
     {
-        public AddNewVehicleValidator()
+        public UpdateVehicleRequestValidator()
         {
-            RuleFor(x => x.UserId)
-               .NotEmpty().WithMessage("UserId is required")
-               .GreaterThan(0).WithMessage("UserId should be greater than 0");
-
             RuleFor(x => x.PlateNumber)
                .NotEmpty().WithMessage("PlateNumber is required")
                .Matches("^(?=.*[A-Z])(?=.*[0-9])[A-Z0-9]+$");
@@ -19,7 +15,7 @@ namespace IWParkingAPI.Middleware.Fluent_Validations.Validators
             .NotEmpty().WithMessage("Type is requred")
             .Must(type => type == "Car" || type == "Adapted Car")
             .WithMessage("Car type must be either 'Car' or 'Adapted Car'");
-          
         }
     }
 }
+
