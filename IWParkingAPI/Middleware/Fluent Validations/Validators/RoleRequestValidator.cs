35dc55613f0e03b4d8f7f90c290a0b76e410bb92
Maja Vuevska <maja.vuevska@gmail.com>
2023-08-17 09:58:44 +0200

    Fluent validation rules for Role

---
+++ IWParkingAPI/Middleware/Fluent Validations/Validators/RoleRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using IWParkingAPI.Models.Requests;
+
+namespace IWParkingAPI.Middleware.Fluent_Validations.Validators
+{
+    public class RoleRequestValidator : AbstractValidator<RoleRequest>
+    {
+        public RoleRequestValidator() 
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required");
+        }
+    }
+}
