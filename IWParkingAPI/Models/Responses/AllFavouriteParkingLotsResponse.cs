5307bbb61d6e305d83760569af7f32c278f63187
Maja Vuevska <maja.vuevska@gmail.com>
2023-09-04 15:51:27 +0200

    Add ParkingLotWithAvailableCapacityDTO response

---
+++ IWParkingAPI/Models/Responses/AllFavouriteParkingLotsResponse.cs
@@ -0,0 +1,10 @@
+﻿using IWParkingAPI.Models.Responses.Dto;
+
+namespace IWParkingAPI.Models.Responses
+{
+    public class AllFavouriteParkingLotsResponse : ResponseBase
+    {
+        public IEnumerable<ParkingLotWithFavouritesDTO>? ParkingLots { get; set; } = new List<ParkingLotWithFavouritesDTO>();
+        public int NumPages { get; set; }
+    }
+}
---
+++ IWParkingAPI/Models/Responses/AllParkingLotResponse.cs
@@ -0,0 +1,10 @@
+﻿using IWParkingAPI.Models.Responses.Dto;
+
+namespace IWParkingAPI.Models.Responses
+{
+    public class AllParkingLotResponse : ResponseBase
+    {
+        public IEnumerable<ParkingLotWithAvailableCapacityDTO>? ParkingLots { get; set; } = new List<ParkingLotWithAvailableCapacityDTO>();
+        public int NumPages { get; set; }
+    }
+}
