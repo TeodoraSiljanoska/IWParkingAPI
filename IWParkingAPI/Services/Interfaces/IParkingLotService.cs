c4e5540f6fb0e8eb6574dc6564fbdd0bc55029a7
Maja Vuevska <maja.vuevska@gmail.com>
2023-09-04 15:53:19 +0200

    Add CalculateCapacityExtension class which caluclates the available capacity in a Parking Lot

--- IWParkingAPI/Services/Interfaces/IParkingLotService.cs
+++ IWParkingAPI/Services/Interfaces/IParkingLotService.cs
@@ -5,13 +5,13 @@ namespace IWParkingAPI.Services.Interfaces
 {
     public interface IParkingLotService
     {
-        public AllParkingLotsResponse GetAllParkingLots(int pageNumber, int pageSize, FilterParkingLotRequest request);
+        public AllParkingLotResponse GetAllParkingLots(int pageNumber, int pageSize, FilterParkingLotRequest request);
         public ParkingLotResponse GetParkingLotById(int id);
         public ParkingLotResponse DeactivateParkingLot(int id);
         public ParkingLotResponse CreateParkingLot(ParkingLotReq request);
         public ParkingLotResponse UpdateParkingLot(int id, UpdateParkingLotRequest changes);
         public ParkingLotResponse MakeParkingLotFavorite(int parkingLotId);
         public ParkingLotResponse RemoveParkingLotFavourite(int parkingLotId);
-        public AllParkingLotsResponse GetUserFavouriteParkingLots(int pageNumber, int pageSize);
+        public AllFavouriteParkingLotsResponse GetUserFavouriteParkingLots(int pageNumber, int pageSize);
     }
 }
